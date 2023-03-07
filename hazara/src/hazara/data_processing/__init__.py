79df06457fd8aebc27c62dfa5147cfe184fe8e77
Ali Ahmadi <ali.ahmadi@siemens.com>
2023-03-07 20:52:21 +0000

    feat: Adding new class to organize data.

---
+++ hazara/src/hazara/data_processing/__init__.py
@@ -0,0 +1,16 @@
+class DataProcessing():
+    def __init__(self, dir) -> None:
+        self.poets = []
+        pass
+    
+    def read_data(self):
+        # Read Every file, and save it in a list
+        # Append to global list of list (every list has one poet)
+        # [[چشم های تو پر سرمه سیاه لبای تو پر خنده موری, عالم تباه شاه پری. بند دل کنده موری], [], [],]
+        poet = []
+        self.poets.append(poet)
+
+    def transform_data(self):
+        # joblib
+        # Pickle
+        pass
